c8a79851fb43709825f2e202fc0fc8a6ab6b76ed
pavan-mannschaft25 <pavankalyan.d@mannschaftit.com>
2026-08-21 17:52:33 +0530

    career page new changes updated

--- src/components/careers/HeroSection.jsx
+++ src/components/careers/HeroSection.jsx
@@ -1,13 +1,24 @@
+// import React, { memo } from "react";
+// import banner from "../../assets/banners/careers_banner.png";
+
+// const HeroSection = memo(() => (
+//   <section className="relative h-[24vh] md:h-[50vh] lg:h-[75vh] xl:h-[70vh]">
+//     <img
+//       src={banner}
+//       alt="Innovise Career"
+//       className="absolute inset-0 w-full h-full object-cover transition-transform duration-[8s] ease-out"
+//     />
+//   </section>
+// ));
+// export default HeroSection;
+
 import React, { memo } from "react";
 import banner from "../../assets/banners/careers_banner.png";
 
 const HeroSection = memo(() => (
-  <section className="relative h-[24vh] md:h-[50vh] lg:h-[75vh] xl:h-[70vh]">
-    <img
-      src={banner}
-      alt="Innovise Career"
-      className="absolute inset-0 w-full h-full object-cover transition-transform duration-[8s] ease-out"
-    />
+  <section className="relative w-full overflow-hidden">
+    <img src={banner} alt="Innovise Career" className="block w-full h-auto" />
   </section>
 ));
+
 export default HeroSection;
